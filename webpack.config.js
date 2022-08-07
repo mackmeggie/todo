d4772e8ada4eb335c005ff6beca78bac667fd894
mackmeggie <mack.meggie13@gmail.com>
2022-08-07 14:25:47 -0600

    complete webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,17 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  }
 };
